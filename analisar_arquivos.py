12207f0febe48ad46066c27b9be9f32ad6a89e46
Markus Luan <markus23_luan@hotmail.com>
2024-11-03 16:06:47 -0300

    Usando constantes globais

--- analisar_arquivos.py
+++ analisar_arquivos.py
@@ -2,12 +2,7 @@ import os
 import datetime
 import argparse
 
-# Códigos de cores ANSI
-RED = '\033[91m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-BLUE = '\033[94m'
-RESET = '\033[0m'
+import constantes
 
 def main (pasta_arquivos_infectados: str, dt_infeccao: datetime.date, extensao: str):
     unidade = os.path.basename(pasta_arquivos_infectados)
@@ -19,14 +14,14 @@ def main (pasta_arquivos_infectados: str, dt_infeccao: datetime.date, extensao: 
     f_seguros = open(f"arquivos_seguros_{unidade}.txt", "w")
     
     # Percorre o diretório e verifica os arquivos
-    print(f"{BLUE}Escaneando arquivos...")
+    print(f"{constantes.BLUE}Escaneando arquivos...")
     for root, _, files in os.walk(pasta_arquivos_infectados):
         for file in files:
             file_path = os.path.join(root, file)
             
             if os.path.islink(file_path):
                if not os.path.exists(os.readlink(file_path)):
-                   print(f"{RED}O arquivo aponta para um link invalido: {file}")
+                   print(f"{constantes.RED}O arquivo aponta para um link invalido: {file}")
                    f_infectados.write(f"{file_path} \n")
                    continue
             
@@ -37,17 +32,17 @@ def main (pasta_arquivos_infectados: str, dt_infeccao: datetime.date, extensao: 
                 dt_modificacao = datetime.datetime.fromtimestamp(stat_info.st_mtime)
                 
                 if (extensao and file.endswith(extensao)) or dt_modificacao >= dt_infeccao:
-                    print(f"{RED}Arquivo infectado localizado: {file} | Criado em: {dt_alteracao_metadados.isoformat()} | Modificado em: {dt_modificacao.isoformat()}")
+                    print(f"{constantes.RED}Arquivo infectado localizado: {file} | Criado em: {dt_alteracao_metadados.isoformat()} | Modificado em: {dt_modificacao.isoformat()}")
                     f_infectados.write(f"{file_path} \n")
                 else:
                     if dt_alteracao_metadados >= dt_infeccao:
-                        print(f"{YELLOW}Os metadados do arquivo foram modificados: {file}")
+                        print(f"{constantes.YELLOW}Os metadados do arquivo foram modificados: {file}")
                         f_meta_alterados.write(f"{file_path} \n")
                     
-                    print(f"{GREEN}O arquivo aparentemente está seguro: {file}")
+                    print(f"{constantes.GREEN}O arquivo aparentemente está seguro: {file}")
                     f_seguros.write(f"{file_path} \n")
             except Exception as e:
-                print(f"{RED}Ocorreu um erro ao analisar o arquivo: {file}\nConsiderando como corrompido\nErro: {e}\n")
+                print(f"{constantes.RED}Ocorreu um erro ao analisar o arquivo: {file}\nConsiderando como corrompido\nErro: {e}\n")
                 f_infectados.write(f"{file_path} \n")
                 continue
     
@@ -55,7 +50,7 @@ def main (pasta_arquivos_infectados: str, dt_infeccao: datetime.date, extensao: 
     f_meta_alterados.close()
     f_seguros.close()
 
-    print(f"{BLUE}Escaneamento concluído!")
+    print(f"{constantes.BLUE}Escaneamento concluído!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Escanea arquivos infectados com RANSON")
@@ -68,9 +63,9 @@ if __name__ == "__main__":
     try:
         dt_infeccao = datetime.datetime.strptime(args.dt_infeccao, "%d/%m/%Y")
     except:
-        raise Exception ("Erro: A data de infecção deverá ser passada nesse formato: DIA/MES/ANO.\nExemplo 26/10/2024!")
+        raise Exception (f"{constantes.RED}Erro: A data de infecção deverá ser passada nesse formato: DIA/MES/ANO.\nExemplo 26/10/2024!")
     
     if not os.path.isdir(args.caminho):
-        raise Exception ("Erro: O caminho informado não existe!\nO caminho deve ser a pasta onde os arquivos infectados estão!")
+        raise Exception (f"{constantes.RED}Erro: O caminho informado não existe!\nO caminho deve ser a pasta onde os arquivos infectados estão!")
     
     main(args.caminho, dt_infeccao, args.extensao)
